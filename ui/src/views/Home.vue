f36a2dcbb1a81be0c0bb1fe2431f60f8148507b6
Brad-- <brad.smith.1324@gmail.com>
2020-06-26 17:49:14 -0600

    CLI create & some pruning of demo UI things

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ComponentBois msg="wassup"></ComponentBois>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ComponentBois from "@/components/ComponentBois.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ComponentBois
+  }
+};
+</script>
